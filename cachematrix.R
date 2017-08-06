476fcdce6c9fcd8d5720633f657ae0a84e1ab05e
APA-DS <daniel.silvis@cga.ct.gov>
2017-08-05 23:26:28 -0400

    Completed Assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  
+  if(!is.null(inv)) {
+    return(inv)
+  }
+  
+  data <- x$get()
+  
+  inv <- solve(data, ...)
+  
+  x$setinverse(inv)
+  
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
